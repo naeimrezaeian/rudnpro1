5db0166fee6b7e2d10fa554e93a275468f885481
naeimrezaeian <naeim.rezaeian@hotmail.com>
2021-07-14 11:26:04 +0300

    Change

--- Models/grouprelation.js
+++ Models/grouprelation.js
@@ -1,46 +1,12 @@
 class GroupRelation{
 
-    constructor(GroupId,TeacherId,SubjectId,Status){        
+    constructor(GroupId,UserId,SubjectId,Status){        
         this.groupId=GroupId;
-        this.teacherId=TeacherId;
+        this.userId=UserId;
         this.subjectId=SubjectId;
         this.status=Status;
     }
 
     
-    getGroupId(){
-        return this.GroupId;
-    }
-    setGroupId(GroupId){
-        this.GroupId=GroupId;
-    }
-    getTeacherId(){
-        return this.TeacherId;
-    }
-    setTeacherId(TeacherId){
-        this.TeacherId=TeacherId;
-    }
-    getStatus(){
-        return this.Status;
-    }
-    setStatus(Status){
-        this.Status=Status;
-    }
-    getSubjectId(){
-        return this.SubjectId;
-    }
-    setSubjectId(SubjectId){
-        this.SubjectId=SubjectId;
-    }
-
-    toJSON(){
-        return {  
-            GroupId : this.GroupId,
-            TeacherId : this.TeacherId,
-            SubjectId:  this.SubjectId,
-            Status : this.Status                
-        }
-    }
-
 }
 module.exports=GroupRelation;
