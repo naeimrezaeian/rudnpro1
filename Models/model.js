b11c409b145f581f4ed369eb05093d41cd541417
naeimrezaeian <naeim.rezaeian@hotmail.com>
2021-07-20 13:32:12 +0300

    MAC 20-7

--- Models/model.js
+++ Models/model.js
@@ -1,11 +1,11 @@
 class Model{
      
     constructor(Title,SubjectId,UrlModel,
-        Parameters,Description,GeneralModel,Status){            
+        Description,GeneralModel,Status){            
             this.title=Title;           
             this.subjectId=SubjectId;
             this.urlModel=UrlModel;
-            this.parameters=Parameters;
+           // this.parameters=Parameters;
             this.description=Description;
             this.generalModel=GeneralModel;
             this.status=Status;
